1dff3b0319ad42a508f0cdf3641641a3c537c7d9
きよたか <c4mo.sw@gmail.com>
2022-12-21 12:09:55 -0500

    First commit

---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useContext } from "react";
+import { taskContext } from "../context/TaskContext";
+
+function TaskForm() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const { createTask } = useContext(taskContext);
+
+  const handleDubmit = (e) => {
+    e.preventDefault();
+    createTask({
+      title,
+      description,
+    });
+    setTitle("");
+    setDescription("");
+  };
+
+  return (
+    <div className="max-w-md mx-auto">
+      <form className="bg-slate-800 p-10 mb-4" onSubmit={handleDubmit}>
+        <h1 className="text-2xl font-bold text-white mb-3">Crea tu tarea</h1>
+        <input
+          className="bg-slate-300 p-3 w-full mb-2"
+          placeholder="Escribe tu tarea"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+          autoFocus
+        />
+        <textarea
+          className="bg-slate-300 p-3 w-full mb-2"
+          placeholder="Descripcion de la tarea"
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
+        ></textarea>
+        <button className="bg-indigo-500 px-3 py-1 text-white hover:bg-indigo-700">
+          Guardar
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default TaskForm;
